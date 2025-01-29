7c643965273a6c59d0d5745175e21a21d16ec8bb
frchp <achille.petitjean@gmail.com>
2025-01-29 14:00:32 +0100

    Revert "Change Rust code to blink to better emulate it"
    This reverts commit 3f61095053e59ef53b032501a962b2d472bf1891.

--- src/main.rs
+++ src/main.rs
@@ -9,75 +9,63 @@ use core::cell::RefCell;
 use core::ops::DerefMut;
 
 // Create mutex to access register during interrupts
-static G_TIM3: Mutex<RefCell<Option<stm32l552::TIM3>>> = Mutex::new(RefCell::new(None));
+static G_EXTI: Mutex<RefCell<Option<stm32l552::EXTI>>> = Mutex::new(RefCell::new(None));
 static G_GPIOC: Mutex<RefCell<Option<stm32l552::GPIOC>>> = Mutex::new(RefCell::new(None));
 
 #[entry]
 fn main() -> ! {
   let peripherals = stm32l552::Peripherals::take().unwrap();
-  let gpioc = peripherals.GPIOC;
+  let gpioc = &peripherals.GPIOC;
   let rcc = &peripherals.RCC;
-  let timer3 = peripherals.TIM3;
+  let exti = &peripherals.EXTI;
 
   // Setup clocks
   rcc.ahb2enr.modify(|_, w| w.gpiocen().set_bit());
   rcc.ahb2enr.read().gpiocen(); // read to let enabling be done
-  rcc.apb1enr1.modify(|_, w| w.tim3en().set_bit());
-  rcc.apb1enr1.read().tim3en(); // read to let enabling be done
 
-  // Setup TIMER3
-  free(|cs|
-    {
-    timer3.cr1.write(|w| w.cen().clear_bit()); // disable timer
-    timer3.psc.write(|w| w.psc().variant(30)); // set prescaler for 500ms period @f 4MHz = MSI @rst
-    timer3.arr.modify(|_, w| w.arr_l().variant(0xFFFF)); // set arr value for 500ms period @f 4MHz = MSI @rst
-    timer3.dier.write(|w| w.uie().set_bit()); // enable update interrupt
-    timer3.cr1.modify(|_, w| w.arpe().set_bit()); // enable arr preload
-    timer3.cr1.modify(|_, w| w.cen().set_bit()); // enable timer
-    timer3.egr.write(|w| w.ug().set_bit()); // update generation
-    // We can no longer use this variable, and instead have to access it via the mutex.
-    G_TIM3.borrow(cs).replace(Some(timer3));
-  });
+  // Setup button PC13
+    // No pull
+  gpioc.pupdr.modify(|_, w| w.pupdr13().floating());
+    // Mode input
+  gpioc.moder.modify(|_, w| w.moder13().input());
+  // Setup EXTI for PC13
+  exti.exticr4.modify(|_, w| w.exti8_15().variant(2)); // Port C
+  exti.imr1.modify(|_, w| w.im13().set_bit());
+  exti.ftsr1.modify(|_, w| w.ft13().set_bit());
+  exti.fpr1.write(|w| w.fpif13().clear_bit());
   // Setup LED PC7
-  free(|cs|
-  {
-    gpioc.otyper.modify(|_, w| w.ot7().push_pull()); // Push pull
-    gpioc.moder.modify(|_, w| w.moder7().output()); // Mode output
-    // We can no longer use this variable, and instead have to access it via the mutex.
-    G_GPIOC.borrow(cs).replace(Some(gpioc));
-  });
+    // Push pull
+  gpioc.otyper.modify(|_, w| w.ot7().push_pull());
+    // Mode output
+  gpioc.moder.modify(|_, w| w.moder7().output());
   // Setup NVIC interrupt
   unsafe
   {
-    NVIC::unmask(Interrupt::TIM3);
+    NVIC::unmask(Interrupt::EXTI13);
   };
+
+  // We can no longer use those variables, and instead have to access it via the mutex.
+  free(|cs| G_GPIOC.borrow(cs).replace(Some(peripherals.GPIOC)));
+  free(|cs| G_EXTI.borrow(cs).replace(Some(peripherals.EXTI)));
 
   loop
   {
-    cortex_m::asm::wfi();
   }
 }
 
 #[interrupt]
-fn TIM3()
+fn EXTI13()
 {
   // Clear interrupt
-  free(|cs|
-  {
-    if let Some(ref mut tim3) = G_TIM3.borrow(cs).borrow_mut().deref_mut()
-    {
-      if tim3.sr.read().uif().bit_is_set()
-      {
-        tim3.sr.modify(|_, w| w.uif().clear_bit()); // Clear update interrupt flag
-      }
+  free(|cs| {
+    if let Some(ref mut exti) = G_EXTI.borrow(cs).borrow_mut().deref_mut() {
+      exti.fpr1.write(|w| w.fpif13().set_bit()); // Bit is cleared by writing 1
     }
   });
 
   // Toggle LED
-  free(|cs|
-  {
-    if let Some(ref mut gpioc) = G_GPIOC.borrow(cs).borrow_mut().deref_mut()
-    {
+  free(|cs| {
+    if let Some(ref mut gpioc) = G_GPIOC.borrow(cs).borrow_mut().deref_mut() {
       if gpioc.odr.read().odr7().bit_is_set()
       {
         gpioc.odr.write(|w| w.odr7().clear_bit());
