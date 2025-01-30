e86b7e8e40ebe98967eba1f71a5c3d6bbf5313e1
frchp <achille.petitjean@gmail.com>
2025-01-30 14:32:28 +0100

    WIP : CAN sniffer
    missing registers in stm32-rs lib

--- src/main.rs
+++ src/main.rs
@@ -1,122 +1,114 @@
-#![no_main]
-#![no_std]
+//! bxCAN to USART2 Example for STM32F407 using Rust
+//! Reads all CAN messages and sends them via USART2 with decoded details
 
-use stm32f4::stm32f407::{self, interrupt, Interrupt, NVIC};
-use panic_halt as _;
+#![no_std]
+#![no_main]
+
 use cortex_m_rt::entry;
-use cortex_m::interrupt::{free, Mutex};
+use cortex_m::interrupt::{self, Mutex};
+use cortex_m::peripheral::NVIC;
+use stm32f4::stm32f407::{self, CAN1, USART2, Interrupt};
 use core::cell::RefCell;
 use core::sync::atomic::{AtomicUsize, Ordering};
+use panic_halt as _;
 
-static G_TIM3: Mutex<RefCell<Option<stm32f407::TIM3>>> = Mutex::new(RefCell::new(None));
-static G_USART2: Mutex<RefCell<Option<stm32f407::USART2>>> = Mutex::new(RefCell::new(None));
-
-// USART TX DATA
-static TX_BUFFER: &[u8] = b"Hello world\r\n";
+static CAN: Mutex<RefCell<Option<CAN1>>> = Mutex::new(RefCell::new(None));
+static USART: Mutex<RefCell<Option<USART2>>> = Mutex::new(RefCell::new(None));
+static TX_BUFFER: Mutex<RefCell<[u8; 64]>> = Mutex::new(RefCell::new([0; 64]));
 static TX_INDEX: AtomicUsize = AtomicUsize::new(0);
+static TX_LENGTH: AtomicUsize = AtomicUsize::new(0);
 
 #[entry]
 fn main() -> ! {
-  let peripherals = stm32f407::Peripherals::take().unwrap();
-  let rcc = &peripherals.RCC;
-  let timer3 = peripherals.TIM3;
-  let usart2 = peripherals.USART2;
+    let dp = stm32f407::Peripherals::take().unwrap();
+    let _cp = cortex_m::Peripherals::take().unwrap();
 
-  // Setup PLL
-  rcc.cr.modify(|_, w| w.hseon().set_bit());
-  while rcc.cr.read().hserdy().bit_is_clear() {}
+    let rcc = &dp.RCC;
+    rcc.apb1enr.modify(|_, w| w.can1en().set_bit().usart2en().set_bit());
 
-  // Configure PLL: HSE as source, multiply by 336, divide by 4 for 84MHz SYSCLK
-  rcc.pllcfgr.modify(|_, w| {
-      w.pllsrc().set_bit() // HSE as PLL source
-        .pllm().variant(8) // VCO input = HSE / 8 (1MHz)
-        .plln().variant(336) // VCO output = 1MHz * 336 = 336MHz
-        .pllp().div4() // PLLP = 4, SYSCLK = 336MHz / 4 = 84MHz
-  });
+    // Configure USART2 (115200 baud, 8N1)
+    let usart2 = &dp.USART2;
+    usart2.brr.modify(|_, w| {
+      w.div_fraction().variant(0)
+        .div_mantissa().variant(45)
+    }); // Assuming PCLK1 at 42MHz, 42_000_000 / 115200 ≈ 364, divide by 8 gives 45
+    usart2.cr1.modify(|_, w| w.ue().set_bit()); // enable usart2
+    usart2.cr1.modify(|_, w| w.te().set_bit()); // transmit enable
+    usart2.cr1.modify(|_, w| w.txeie().set_bit()); // TX interrupt
 
-  // Enable PLL
-  rcc.cr.modify(|_, w| w.pllon().set_bit());
-  while rcc.cr.read().pllrdy().bit_is_clear() {};
+    // Configure CAN1
+    let can1 = &dp.CAN1;
+    can1.mcr.modify(|_, w| w.inrq().set_bit()); // Enter initialization mode
+    while can1.msr.read().inak().bit_is_clear() {}
+    can1.btr.modify(|_, w| w.silm().clear_bit()); // normal operation
+    can1.btr.modify(|_, w| w.lbkm().clear_bit()); // loop back mode disabled
+    can1.btr.modify(|_, w| w.brp().variant(3)); // t_q = BRP+1 * t_pclk
+    can1.btr.modify(|_, w| w.ts1().bits(6)); // t_bs1 = t_q * TS1+1
+    can1.btr.modify(|_, w| w.ts2().bits(1)); // t_bs2 = t_q * TS2+1
+    can1.mcr.modify(|_, w| w.inrq().clear_bit()); // Exit initialization mode
+    while can1.msr.read().inak().bit_is_set() {}
+    can1.ier.modify(|_, w| w.fmpie0().set_bit()); // Enable FIFO 0 message pending interrupt
 
-  // Select PLL as system clock
-  rcc.cfgr.modify(|_, w| w.sw().pll());
-  while !rcc.cfgr.read().sws().is_pll() {};
-
-  // Enable peripherals clocks
-  rcc.apb1enr.modify(|_, w| w.tim3en().set_bit());
-  rcc.apb1enr.modify(|_, w| w.usart2en().set_bit());
-
-  // Setup USART2
-  usart2.brr.modify(|_, w| {
-    w.div_fraction().variant(0)
-      .div_mantissa().variant(45)
-  }); // Assuming PCLK1 at 42MHz, 42_000_000 / 115200 ≈ 364, divide by 8 gives 45
-  usart2.cr1.modify(|_, w| w.ue().set_bit().te().set_bit()); // Enable USART and transmitter
-  free(|cs|
-    {
-      G_USART2.borrow(cs).replace(Some(usart2));
+    interrupt::free(|cs| {
+        CAN.borrow(cs).replace(Some(dp.CAN1));
+        USART.borrow(cs).replace(Some(dp.USART2));
     });
-    // Setup TIMER3 with APB clock default (/1, 16MHz @ rst)
-  timer3.psc.write(|w| w.psc().variant(1599)); // Prescaler: 16MHz / (1599 + 1) = 10kHz (0.1ms per tick)
-  timer3.arr.modify(|_, w| w.arr().variant(4999)); // Auto-reload: 5000 ticks = 500ms interval
-  timer3.dier.write(|w| w.uie().set_bit()); // enable update interrupt
-  timer3.egr.write(|w| w.ug().set_bit()); // update generation
-  timer3.cr1.modify(|_, w| w.cen().set_bit()); // enable timer
-  free(|cs|
-  {
-    G_TIM3.borrow(cs).replace(Some(timer3));
-  });
-  // Setup NVIC interrupt
-  unsafe
-  {
-    NVIC::unmask(Interrupt::TIM3);
-    NVIC::unmask(Interrupt::USART2);
-  };
 
-  loop
-  {
-    cortex_m::asm::wfi();
-  }
-}
-
-#[interrupt]
-fn TIM3()
-{
-  // Clear interrupt
-  free(|cs|
-  {
-    if let Some(ref mut tim3) = G_TIM3.borrow(cs).borrow_mut().as_mut()
-    {
-      if tim3.sr.read().uif().bit_is_set()
-      {
-        tim3.sr.modify(|_, w| w.uif().clear_bit()); // Clear update interrupt flag
-        TX_INDEX.store(1, Ordering::Release); // Reset buffer index at 1 as we send the 0 idx here
-        if let Some(ref mut usart2) = G_USART2.borrow(cs).borrow_mut().as_mut()
-        {
-          usart2.cr1.modify(|_, w| w.txeie().set_bit()); // Enable TXE interrupt
-          usart2.dr.write(|w| w.dr().bits(TX_BUFFER[0] as u16)); // Start transmission
-        }
-      }
+    unsafe {
+        NVIC::unmask(Interrupt::CAN1_RX0);
+        NVIC::unmask(Interrupt::USART2);
     }
-  });
+
+    loop {}
 }
 
-#[interrupt]
-fn USART2() {
-    free(|cs|
-      {
-        if let Some(ref mut usart2) = G_USART2.borrow(cs).borrow_mut().as_mut()
-        {
-          let index = TX_INDEX.load(Ordering::Acquire);
-          if index < TX_BUFFER.len()
-          {
-            usart2.dr.write(|w| w.dr().bits(TX_BUFFER[index] as u16));
+#[cortex_m_rt::interrupt]
+fn stm32f4::stm32f407::Interrupt::CAN1_RX0() {
+    interrupt::free(|cs| {
+        if let Some(ref mut can1) = CAN.borrow(cs).borrow_mut().as_mut() {
+            if can1.rfr[0].read().fmp().bits() > 0 {
+                let rir = can1.rfr[0].read().bits();
+                let id = if rir & (1 << 2) != 0 {
+                    rir >> 3 // Extended ID
+                } else {
+                    (rir >> 21) & 0x7FF // Standard ID
+                };
+                let dlc = (can1.rdtr0.read().dlc().bits() & 0xF) as usize;
+                let data = [
+                    can1.rfr[0].read().bits() as u8,
+                    (can1.rfr[0].read().bits() >> 8) as u8,
+                    (can1.rfr[0].read().bits() >> 16) as u8,
+                    (can1.rfr[0].read().bits() >> 24) as u8,
+                    can1.rfr[0].read().bits() as u8,
+                    (can1.rfr[0].read().bits() >> 8) as u8,
+                    (can1.rfr[0].read().bits() >> 16) as u8,
+                    (can1.rfr[0].read().bits() >> 24) as u8,
+                ];
+                let mut buffer = TX_BUFFER.borrow(cs).borrow_mut();
+                let len = core::fmt::Write::write_fmt(&mut *buffer, format_args!(
+                    "ID: {:X} DLC: {} DATA: {:?}\r\n", id, dlc, &data[..dlc]
+                )).unwrap_or(0);
+                TX_LENGTH.store(len, Ordering::Release);
+                TX_INDEX.store(0, Ordering::Release);
+                can1.rfr[0].modify(|_, w| w.rfom0().set_bit()); // Release FIFO 0
+                USART.borrow(cs).borrow_mut().as_mut().unwrap().cr1.modify(|_, w| w.txeie().set_bit());
+            }
+        }
+    });
+}
+
+#[cortex_m_rt::interrupt]
+fn stm32f4::stm32f407::Interrupt::USART2() {
+    interrupt::free(|cs| {
+        let index = TX_INDEX.load(Ordering::Acquire);
+        let length = TX_LENGTH.load(Ordering::Acquire);
+        if index < length {
+            let usart2 = USART.borrow(cs).borrow_mut().as_mut().unwrap();
+            let buffer = TX_BUFFER.borrow(cs).borrow();
+            usart2.dr.write(|w| w.dr().bits(buffer[index] as u16));
             TX_INDEX.store(index + 1, Ordering::Release);
-          }
-          else
-          {
-            usart2.cr1.modify(|_, w| w.txeie().clear_bit()); // Disable TX interrupt when done
-          }
+        } else {
+            USART.borrow(cs).borrow_mut().as_mut().unwrap().cr1.modify(|_, w| w.txeie().clear_bit());
         }
     });
 }
